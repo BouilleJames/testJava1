5cc1825ab8e1fc232493e213e342d5b6e5f68d5c
BouilleJames <77192627+BouilleJames@users.noreply.github.com>
2023-05-17 12:21:19 +0200

    add

--- App.java
+++ App.java
@@ -8,16 +8,19 @@ public class App {
         Voiture mavoiture = new Voiture("Renault", "Grise", 4);
         Moto mamoto = new Moto("Honda", "Noir", 2);
         Moto mamoto1 = new Moto("Kawasaki", "Verte", 2);
-        afficher(mavoiture,mamoto,mamoto1);
+        PoidsLourd camion = new PoidsLourd("Renault", "Rouge", 6);
+        afficher(mavoiture,mamoto,mamoto1,camion);
     }
 
-    public static void afficher(Voiture mavoiture,Moto mamoto, Moto mamoto1) {
+    public static void afficher(Voiture mavoiture,Moto mamoto, Moto mamoto1, PoidsLourd camion) {
         System.out.println("Ce vehicule est une " + mavoiture.getmMarque() + " ,est de couleur " + mavoiture.getmCouleur()
                 + " et a " + mavoiture.getmNbRoues() + " roues");
         System.out.println("Ce vehicule est une " + mamoto.getmMarque() + " ,est de couleur " + mamoto.getmCouleur()
                 + " et a " + mamoto.getmNbRoues() + " roues");
         System.out.println("Ce vehicule est une " + mamoto1.getmMarque() + " ,est de couleur " + mamoto1.getmCouleur()
                 + " et a " + mamoto1.getmNbRoues() + " roues");
+        System.out.println("Ce Poids-Lourd est un " + camion.getmMarque() + " ,est de couleur " + camion.getmCouleur()
+                + " et a " + camion.getmNbRoues() + " roues");
         // System.out.println("la marque est : " + g.getmMarque() + " de couleur : " +
         // g.getmCouleur());
 
---
+++ transport/PoidsLourd.java
@@ -0,0 +1,9 @@
+package transport;
+
+import mere.Vehicule;
+
+public class PoidsLourd extends Vehicule{
+    public PoidsLourd(String marque, String couleur, int nbRoues) {
+        super(marque, couleur, nbRoues);
+    }
+}
