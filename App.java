035005614aa7b6ae9feb8dbd3447e88746ccd8bc
BouilleJames <77192627+BouilleJames@users.noreply.github.com>
2023-05-17 10:58:15 +0200

    add modif import

--- App.java
+++ App.java
@@ -1,20 +1,23 @@
 //import java.util.Scanner;
-import transport.Voiture;
-import transport.Moto;
+import transport.*;
+
 
 public class App {
 
     public static void main(String[] args) {
         Voiture mavoiture = new Voiture("Renault", "Grise", 4);
         Moto mamoto = new Moto("Honda", "Noir", 2);
-        afficher(mavoiture,mamoto);
+        Moto mamoto1 = new Moto("Kawasaki", "Verte", 2);
+        afficher(mavoiture,mamoto,mamoto1);
     }
 
-    public static void afficher(Voiture mavoiture,Moto mamoto) {
+    public static void afficher(Voiture mavoiture,Moto mamoto, Moto mamoto1) {
         System.out.println("Ce vehicule est une " + mavoiture.getmMarque() + " ,est de couleur " + mavoiture.getmCouleur()
                 + " et a " + mavoiture.getmNbRoues() + " roues");
         System.out.println("Ce vehicule est une " + mamoto.getmMarque() + " ,est de couleur " + mamoto.getmCouleur()
                 + " et a " + mamoto.getmNbRoues() + " roues");
+        System.out.println("Ce vehicule est une " + mamoto1.getmMarque() + " ,est de couleur " + mamoto1.getmCouleur()
+                + " et a " + mamoto1.getmNbRoues() + " roues");
         // System.out.println("la marque est : " + g.getmMarque() + " de couleur : " +
         // g.getmCouleur());
 
